8c814dc4a0eea2fe672db82ecdbeb99e5997f6a3
absurd100 <akunpetarunggila@gmail.com>
2026-01-19 15:00:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram import Client
 
 load_dotenv()
-dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
-dns.resolver.default_resolver.nameservers = ['8.8.8.8', '8.8.4.4', '1.1.1.1']
+resolver = dns.resolver.Resolver()
+resolver.nameservers = ['8.8.8.8', '8.8.4.4']
+dns.resolver.default_resolver = resolver
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URL = os.environ.get("MONGO_URL")
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "").strip()
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "").strip()
+MONGO_URL = os.environ.get("MONGO_URL", "").strip()
 OWNER_ID = int(os.environ.get("OWNER_ID", 0))
 
 mongo_client = AsyncIOMotorClient(MONGO_URL)
